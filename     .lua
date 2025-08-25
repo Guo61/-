2939401d76b3b03cf095122a4cb38aa96ed28b18
Guo61 <gg1457o@qq.com>
2025-08-25 16:20:37 +0800

    Add files via upload

---
+++     .lua
@@ -0,0 +1,46 @@
+local OrionLib = loadstring(game:HttpGet('https://pastebin.com/raw/xLRUSiKx'))()
+local Window = OrionLib:MakeWindow
+({Name = "Cat",
+
+HidePremium = false,
+SaveConfig = true,
+IntroText = "欢迎使用Cat Hub",
+ConfigFolder = "520"})
+
+local Tab = Window:MakeTab({
+Name = "速度传奇",
+
+Icon = "rbxassetid://108453362719381",	PremiumOnly = false})
+
+Tab:AddButton({
+Name = "钻石",
+
+Callback = function()
+
+while true do
+local args = {
+	"collectOrb",
+	"Gem",
+	"City"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+wait(0.5)
+end
+end})      
+
+Tab:AddButton({
+
+Name="步",
+
+Callback = function()
+
+while true do
+local args = {
+	"collectOrb",
+	"Orange Orb",
+	"City"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("orbEvent"):FireServer(unpack(args))
+wait(0.5)
+end
+end})
